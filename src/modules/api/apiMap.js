c86a98c42cdb398cf85f1ca370b217d1329ca12c
Sungwoo Wee <wooos91@gmail.com>
2022-03-18 18:25:15 +0900

    Modify apiMap.js
    Home.vue에서 사용할 API 추가

--- src/modules/api/apiMap.js
+++ src/modules/api/apiMap.js
@@ -2,6 +2,7 @@
 
 let apiMap = {
 	// Banner
+	HOME_DATA:	{ method: "get",	useAuth:false,	name: "homeData.asp" },		// 홈의 컨텐츠 정보 API
 	BANNER_LIST:	{ method: "get",	useAuth:false,	name: "banner/bannerList.asp" },		// 배너 목록
 	
 	// File
