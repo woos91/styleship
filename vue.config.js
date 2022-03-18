eaafcfc20c5e4b5b88bd001fea01a4087e48e63a
Sungwoo Wee <wooos91@gmail.com>
2022-03-18 15:12:24 +0900

    Modify vue.config.js
    Base URL설정 추가

--- vue.config.js
+++ vue.config.js
@@ -5,14 +5,16 @@ module.exports = defineConfig({
         loaderOptions: {
             sass: {
                 additionalData: `
-        @import "@/assets/sass/_variables.scss";
-        @import "@/assets/sass/_mixin.scss";
-        @import "@/assets/sass/_opt-default.scss";
-        @import "@/assets/sass/_normalize.scss";
-      `
+                    @import "@/assets/sass/_variables.scss";
+                    @import "@/assets/sass/_mixin.scss";
+                    @import "@/assets/sass/_opt-default.scss";
+                    @import "@/assets/sass/_normalize.scss";
+                `,
             }
         }
-    }
+    },
+    publicPath: process.env.BASE_URL,             // 실제 서비스 될 루트 경로 설정 (env세팅에 따름 : '/styleship/')
+    outputDir: 'dist/styleship'              // 아웃풋($ yarn serveemd...)의 경로 설정 (env세팅에 따름 : '/styleship/')
 });
 
 
