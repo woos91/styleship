2cbd3f383114da33ccb8dc6752416f22227dfd37
Sungwoo Wee <wooos91@gmail.com>
2022-03-18 18:26:44 +0900

    Modify modules/api/auth/getToken.js
    오류 부분 수정

--- src/modules/api/auth/getToken.js
+++ src/modules/api/auth/getToken.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import getLS from '@/plugins/tools/getLS'
+import getLS from '@/modules/tools/getLS'
 
 let getToken = ()=>{    //  로컬스토리지를 읽어서 현재 유효한 인증토큰이 있는지 여부만 파악하여 이를 전달 해주는 매서드
     let _auth = getLS("app_auth");  //  로컬스토리지의 "app_auth" 값 불러오기
